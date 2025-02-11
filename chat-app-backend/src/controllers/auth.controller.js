9d761ef284c5f773831222aecd7a66e8f72132c8
zol22 <zol2295@live.com>
2025-02-11 14:44:08 -0500

    signup function done

--- chat-app-backend/src/controllers/auth.controller.js
+++ chat-app-backend/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import { registerUser, findUser } from "../services/auth.service.js";
 import bycrypt from "bcrypt";
+import { generateToken } from "../utils/generateToken.js";
 
 // Apply strong password policies, both for ops and in-application user management 
 export const signup = async (req, res) => {
@@ -30,15 +31,15 @@ export const signup = async (req, res) => {
 
             // Check if user was created successfully
             if (newUser) {
-                
+                generateToken(newUser.id, res);
                 res.status(201).json({ message: 'User registered successfully', user: newUser });
             } else {
-                return res.status(400).json({ error: 'User registration failed' }); 
+                return res.status(400).json({ error: 'Invalid User Data' }); 
             }
         }
 
     } catch (error) {
-        res.status(400).json({ error: 'User registration failed', details : error.message });
+        res.status(500).json({ error: 'Internal Sever Error', details : error.message });
     }
 }
 
@@ -46,9 +47,35 @@ export const signup = async (req, res) => {
 // On login success, return a JWT token that the client can use to authenticate future requests
 // Auth rate limiting: Disallow more than X login attempts (including password recovery, etc.) in a period of Y
 
-export const login = (req, res) => {
+export const login = async (req, res) => {
 
-    res.send("Login route");
+    const {email, password } = req.body;
+
+    // Validate Incoming Data from  SQL injection and XSS attacks
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Email and Password are required' });
+    }
+
+    try {
+        // Check if user is already registered
+        const user = await findUser(email);
+        if (user) {
+            // Compare the hashed password with the password entered by the user
+            const isMatch = await bycrypt.compare(password, user.password);
+            if (isMatch) {
+                generateToken(user.id, res);
+                res.status(200).json({ message: 'User logged in successfully', user: user });
+            } else {
+                return res.status(400).json({ error: 'Invalid Email or Password' });
+            }   
+        } else {
+            return res.status(400).json({ error: 'Invalid Email or Password' });
+        }
+        
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Sever Error', details : error.message });
+    }
+
 }
 
 export const logout = (req, res) => {
